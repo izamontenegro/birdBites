b8d79a380b8d2f150d32681f45ce2a56c2c0c4f9
izadora Montenegro <izadoraoamontenegro@gmail.com>
2025-01-15 10:09:03 -0300

    style: melhorar exibição dos dados na tela

--- View/ContentView.swift
+++ View/ContentView.swift
@@ -9,35 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var mqttManager = MQTTManager()
-
+    
     var body: some View {
         VStack {
-            Text("Status do Comedouro")
-                .foregroundStyle(.pink)
-                .font(.largeTitle)
-                .padding()
-
-            if let data = mqttManager.receivedData {
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("Luminosidade: \(data.luminosidade)")
-                    Text("Temperatura Externa: \(data.temperaturaExterno) °C")
-                    Text("Umidade Externa: \(data.umidadeExterno) %")
-                    Text("Temperatura Interna: \(data.temperaturaInterno) °C")
-                    Text("Umidade Interna: \(data.umidadeInterno) %")
-                    Text("Quantidade de Comida: \(data.quantidadeComida) %")
-                }
-                .padding()
-            } else {
-                Text("Aguardando dados...")
-                    .foregroundColor(.gray)
-                    .padding()
-            }
-        }
-        .onAppear {
-            mqttManager.configureMQTT()
-        }
-        .onDisappear {
-            mqttManager.disconnect()
+            Text("hello")
         }
     }
 }
--- birdBites/birdBitesApp.swift
+++ birdBites/birdBitesApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct birdBitesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MockDataView()
         }
     }
 }
